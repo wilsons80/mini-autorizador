12ee0c82d1e6706ada101529af072e41b907db1e
Wilson Souza <wilsons80@gmail.com>
2022-11-14 23:25:24 -0300

    Adicionando configurações ao projeto

--- src/main/java/br/com/vr/MiniAutorizadorApplication.java
+++ src/main/java/br/com/vr/MiniAutorizadorApplication.java
@@ -1,5 +1,7 @@
 package br.com.vr;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniAutorizadorApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(MiniAutorizadorApplication.class, args);
 	}
 
