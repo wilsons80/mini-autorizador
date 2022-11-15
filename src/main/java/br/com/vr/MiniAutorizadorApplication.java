b564f5b406dcf0a5e081bd823ef183c4d4d0542a
Wilson Souza <wilsons80@gmail.com>
2022-11-14 23:11:47 -0300

    Criação do projeto

---
+++ src/main/java/br/com/vr/MiniAutorizadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.vr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorApplication.class, args);
+	}
+
+}
